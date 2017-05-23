ac2a0c763c522615470d727fa74d49b985e09571
Scott Winkelmann <scottlandart@gmail.com>
2017-05-23 11:46:25 +0200

    optimized imports

--- pocket.py
+++ pocket.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-import os
+from os import path
 from pocketsphinx import LiveSpeech, get_model_path
 
 model_path = get_model_path()
@@ -11,9 +11,9 @@ speech = LiveSpeech(
     buffer_size=2048,
     no_search=False,
     full_utt=False,
-    hmm=os.path.join(model_path, 'fr'),
-    lm=os.path.join(model_path, 'fr.lm.bin'),
-    dic=os.path.join(model_path, 'cmudict-fr.dict')
+    hmm=path.join(model_path, 'fr'),
+    lm=path.join(model_path, 'fr.lm.bin'),
+    dic=path.join(model_path, 'cmudict-fr.dict')
 )
 
 for phrase in speech:
